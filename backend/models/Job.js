0856faafd8dabe57e62a6796bc0afd9aad6dcd3e
Pragati Jindal <pragatijindal123@gmail.com>
2026-06-19 16:59:54 +0530

    feat: created models folder and Job.js file

---
+++ backend/models/Job.js
@@ -0,0 +1,12 @@
+import mongoose from 'mongoose';
+
+const jobSchema = new mongoose.Schema({
+  title: String,
+  company: String,
+  location: String,
+  description: String,
+  salary: String
+});
+
+const Job = mongoose.model('Job', jobSchema);
+export default Job;
